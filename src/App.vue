847fda0b1c3c13033cc274fbef81b03ffafcba4c
dat96 <dat96dt@gmail.com>
2020-01-30 23:57:12 +0700

    todo el v1

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,22 @@
 <template>
   <div id="app">
-    <h3>Todo Vue.js App</h3>
-    <ul class="list-group">
-      <TodoItem
-        v-for="(data,index) in list_item"
-        :key="index"
-        :data="data"
-        :index="index"
-        @removeTodo="removeTodo"
-        @editTodo="editTodo"
-      />
-    </ul>
-    <el-input placeholder="Please input" v-model="input_todo">
-      <template slot="append">
-        <el-button type="primary" @click="addTodo">Add Todo</el-button>
-      </template>
-    </el-input>
+    <add-todo />
+    <filter-todo />
+    <todo-item />
   </div>
 </template>
 
 <script>
 import TodoItem from "./components/TodoItem.vue";
+import AddTodo from "./components/AddTodo.vue";
+import FilterTodo from "./components/FilterTodo";
 
 export default {
   name: "app",
   components: {
-    TodoItem
-  },
-  data: function() {
-    return {
-      list_item: [{ name: "item1" }, { name: "item2" }, { name: "item3" }],
-      input_todo: ""
-    };
-  },
-  methods: {
-    addTodo: function() {
-      this.list_item.push({
-        name: this.input_todo
-      });
-      this.input_todo = "";
-    },
-    removeTodo: function(index) {
-      this.list_item.splice(index, 1);
-    },
-    editTodo:function(data, index){
-      this.list_item.splice(index,1,{name: data})
-    }
+    TodoItem,
+    AddTodo,
+    FilterTodo
   }
 };
 </script>
@@ -55,7 +26,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
   width: 50%;
